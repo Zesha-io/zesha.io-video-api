2596d236a517fbe987d71dfd2dc443476cca3ab5
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-05-20 15:55:24 +0400

    Zesha Video Manipulation API for Theta

---
+++ server.js
@@ -0,0 +1,215 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const staticFFMPEG = require("ffmpeg-static");
+const { path: staticFfmprobe } = require("ffprobe-static");
+
+const {
+    S3Client,
+    PutObjectCommand,
+    GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const multer = require("multer");
+const upload = multer({ dest: "tmp/" });
+const uuidv4 = require("uuid").v4;
+const slugify = require("slugify");
+const fs = require("fs");
+const fetch = require("node-fetch-commonjs");
+const extractFrames = require("ffmpeg-extract-frames");
+const ffmpeg = require("fluent-ffmpeg");
+
+require("dotenv").config();
+
+const port = 8090;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+console.log(staticFFMPEG);
+console.log(staticFfmprobe);
+
+ffmpeg.setFfmpegPath(staticFFMPEG);
+ffmpeg.setFfprobePath(staticFfmprobe);
+
+// const corsOptions = {
+//     origin: process.env.ZESHA_WEB_URL,
+//     optionsSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
+
+app.all("*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+
+const s3Client = new S3Client({
+    forcePathStyle: false,
+    endpoint: "https://nyc3.digitaloceanspaces.com",
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: process.env.DO_SPACES_KEY,
+        secretAccessKey: process.env.DO_SPACES_SECRET,
+    },
+});
+
+const type = upload.single("video");
+
+app.post("/api/space-upload", type, async (req, res) => {
+    const { namespace } = req.body;
+
+    const video = req.file;
+    if (!video) {
+        return response.status(400).json({
+            error: "No video file found",
+        });
+    }
+
+    const key = `${namespace}/${uuidv4()}-${slugify(video.originalname)}`;
+    const params = {
+        Bucket: process.env.DO_SPACES_BUCKET,
+        Key: key,
+        Body: fs.createReadStream(video.path),
+        ACL: "public-read",
+    };
+
+    try {
+        const data = await s3Client.send(new PutObjectCommand(params));
+
+        return res.status(200).json({ key });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Error uploading video " });
+    }
+});
+
+app.post("/api/generate-signed-url", type, async (req, res) => {
+    const { key, namespace } = req.body;
+
+    const bucketParams = {
+        Bucket: process.env.DO_SPACES_BUCKET,
+        Key: key,
+    };
+
+    try {
+        const url = await getSignedUrl(
+            s3Client,
+            new GetObjectCommand(bucketParams),
+            { expiresIn: 2 * 60 * 60 }
+        ); // 1hr expiration.
+
+        return res.status(200).json({ url });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Error generating presigned url" });
+    }
+});
+
+//@Todo: Clean up tmp files
+//Thumbnail & Length
+app.post("/api/get-video-metadata", type, async (req, res) => {
+    const { url } = req.body;
+
+    const name = `thumbnail-${uuidv4()}.jpg`;
+    const thumbnail = `./public/${name}`;
+
+    try {
+        await extractFrames({
+            input: url,
+            output: thumbnail,
+            offsets: [3500],
+        });
+
+        ffmpeg(url).ffprobe(function (err, metadata) {
+            if (err) {
+                console.log(err);
+                return res
+                    .status(500)
+                    .json({ error: "Error generating metadata" });
+            }
+
+            const { duration, size, tags, format_name } = metadata.format;
+
+            return res
+                .status(200)
+                .json({ thumbnail: name, duration, size, tags, format_name });
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Error generating metadata" });
+    }
+});
+
+app.post("/api/upload-video-to-theta", type, async (req, res) => {
+    const { url, nft_collection } = req.body;
+
+    try {
+        const result = await fetch("https://api.thetavideoapi.com/video", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-tva-sa-id": process.env.THETA_API_KEY,
+                "x-tva-sa-secret": process.env.THETA_API_SECRET,
+            },
+            body: JSON.stringify({
+                source_uri: url,
+                playback_policy: "public",
+                nft_collection: nft_collection,
+            }),
+        });
+
+        const data = await result.json();
+
+        if (!result.ok) {
+            console.log(data.error);
+
+            return res.status(500).json({ error: data.error });
+        }
+
+        return res.status(200).json({ video_id: data.body?.videos[0]?.id });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Error encoding video" });
+    }
+});
+
+app.get("/api/video-transcoding-status", type, async (req, res) => {
+    const { video_id } = req.query;
+
+    try {
+        const result = await fetch(
+            `https://api.thetavideoapi.com/video/${video_id}`,
+            {
+                method: "GET",
+                headers: {
+                    "x-tva-sa-id": process.env.THETA_API_KEY,
+                    "x-tva-sa-secret": process.env.THETA_API_SECRET,
+                },
+            }
+        );
+
+        const data = await result.json();
+
+        if (!result.ok) {
+            console.log(data.error);
+
+            return res.status(500).json({ error: data.error });
+        }
+
+        return res.status(200).json({
+            video_id: data.body?.videos[0]?.id,
+            state: data.body?.videos[0]?.state,
+            progress: data.body?.videos[0]?.progress,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Cannot get video encoding status" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
